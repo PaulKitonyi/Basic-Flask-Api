2fa81a63b88a5b16a0bf49378b7cdc2fbac37265
Paul Kitonyi <paulmucimah@gmail.com>
2018-05-16 18:08:08 +0300

    updated scripts

--- app.py
+++ app.py
@@ -16,6 +16,12 @@ def returnOne(name):
     langs =[language for language in languages if language['name'] == name]
     return jsonify({'language' : langs[0]})
 
+@app.route('/lang', methods=['POST'])
+def addOne():
+    language = {'name' : request.json['name']}
+    languages.append(language)
+    return jsonify({'languages' : languages})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
