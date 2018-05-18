07934b2189e6a51bf308a207349f0da7f0a931d6
Paul Kitonyi <paulmucimah@gmail.com>
2018-05-18 16:17:54 +0300

    updated app.py script

--- app.py
+++ app.py
@@ -9,31 +9,31 @@ def test():
     return jsonify({'message': 'It has worked!!!'})
 
 @app.route('/lang', methods=['GET'])
-def returnAll():
+def return_all():
     return jsonify({'languages' : languages})
 
 @app.route('/lang/<string:name>', methods=['GET'])
-def returnOne(name):
+def return_one(name):
     langs =[language for language in languages if language['name'] == name]
     return jsonify({'language' : langs[0]})
 
 # POST Method
 @app.route('/lang', methods=['POST'])
-def addOne():
+def add_one():
     language = {'name' : request.json['name']}
     languages.append(language)
     return jsonify({'languages' : languages})
 
 # PUT Method
 @app.route('/lang/<string:name>', methods=['PUT'])
-def editOne(name):
+def edit_one(name):
     langs = [language for language in languages if language['name'] == name]
     langs[0]['name'] = request.json['name']
     return jsonify({'language' : langs[0]})
 
 #Delete Method
 @app.route('/lang/<string:name>', methods=['DELETE'])
-def removeOne(name):
+def remove_one(name):
     lang = [language for language in languages if language['name'] == name]
     languages.remove(lang[0])
     return jsonify({'languages': languages })
